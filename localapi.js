29ec677bd3b163fd8d33153a1b1b83b91b7e3bf8
Hakan ÖZGÜR <hakan@ait.com.tr>
2021-10-17 15:33:36 +0300

    initial commit

---
+++ localapi.js
@@ -0,0 +1,41 @@
+const { networkInterfaces } = require('os');
+const fs = require('fs');
+const path = require('path');
+
+let netmask = '255,255,255,0';
+let gateway = '192,168,2,1';
+
+const configfilename = 'app-config.json';
+const configpath = path.join(__dirname, configfilename);
+
+function getLocalIPV4() {
+    const interfaces = networkInterfaces();
+    try {
+        const config = JSON.parse(fs.readFileSync(configpath));
+        if(config) {
+            netmask = config.netmask || '255,255,255,0';
+            gateway = config.gateway || '192,168,2,1';
+        }
+        else {
+            console.log('!app-config.json file not found. Using default values.');
+        }
+    }
+    catch (e) {
+        console.log('app-config.json file not found. Using default values.');
+    }
+    for (let k in interfaces) {
+        for (let k2 in interfaces[k]) {
+            const address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal && address.netmask === netmask) {
+                return address.address;
+            }
+        }
+    }
+    return null;
+}
+
+module.exports = {
+    getLocalIPV4,
+    gateway,
+    netmask,
+};
---
+++ test.js
@@ -0,0 +1,3 @@
+const localapi = require('./localapi.js');
+
+console.log("Your Local IP V4 is",localapi.getLocalIPV4());
